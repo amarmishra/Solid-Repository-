fa019b1717d9ff47c8ed2dee1b5ee53d1b9930ac
amarmishra <amar.m130@gmail.com>
2024-04-25 04:49:49 +0530

    changed render db host to internal

--- src/connect-db.ts
+++ src/connect-db.ts
@@ -4,7 +4,7 @@ import path from 'path'
 export const AppDataSource =
     new DataSource({
         type: "postgres",
-        host: "dpg-coko4egl5elc73ddjsv0-a.singapore-postgres.render.com",
+        host: "dpg-coko4egl5elc73ddjsv0-a",
         port: 5432,
         username: 'amarmishra',
         password: "SDZd6eEuTWIHXAc7ebwdJk36nIXOyhl6",
